055e9d13417e8d0b06ba85cc42672831f81a891d
arpit-2727 <arpit1111k@gmail.com>
2024-10-07 11:57:00 +0530

    File Colocation

--- src/app/products/[productId]/reviews/[reviewId]/page.js
+++ src/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,24 +1,20 @@
 import React from 'react'
-import NotFound from '@/app/not-found'
-import { notFound } from 'next/navigation'
-
-
+import { notFound } from 'next/navigation';
 
 export default function ReviewDetail({ params }) {
   return (
     <>
-      {parseInt(params.reviewId) > 1000 ? notFound() : (
-        <h1>
-          Review {params.reviewId} for Product {params.productId}
-
-         </h1>
-
-
-        
+      {parseInt(params.reviewId) > 1000 ? (
+        <div>
+          <h1>Review Not Found</h1>
+          <p>Please go back to the product page.</p>
+        </div>
+      ) : (
+        <div>
+          <h1>Review {params.reviewId} for Product {params.productId}</h1>
+        </div>
       )}
     </>
   );
-  
 }
-
 
