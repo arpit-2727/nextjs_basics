2fc1346175748b9642f164456d5528535472602a
arpit-2727 <arpit1111k@gmail.com>
2024-10-13 12:28:47 +0530

    Error

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.js
@@ -0,0 +1,5 @@
+"use client"
+
+export default function ErrorBoundary(error){
+    return <h1>Error Occured</h1>;
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.js
+++ src/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { notFound } from 'next/navigation';
-
+function getRandomInt(){
+  return Math.floor(Math.random() * 1001);
+}
 export default function ReviewDetail({ params }) {
+  const random = getRandomInt(2);
+  if(random % 2 === 1){
+    throw new Error ("Error Loading Review")
+  }
   return (
     <>
       {parseInt(params.reviewId) > 1000 ? (
