a03bd1070b8a8355f916499037fa4e3a218cc25e
arpit-2727 <arpit1111k@gmail.com>
2024-10-16 11:53:45 +0530

    Route handler - GET

---
+++ src/app/comments/data.js
@@ -0,0 +1,13 @@
+export const comments =[
+    {
+        id: 1,
+        text: 'This is a great post!',
+    },
+    {   id: 2,
+        text: 'I agree!',
+     },
+    {
+        id: 3,
+        text: 'I disagree with your opinion.',
+    },
+];
