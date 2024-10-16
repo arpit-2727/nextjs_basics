25d5037f908a84b5519bbd23399ca62ba4c540dc
arpit-2727 <arpit1111k@gmail.com>
2024-10-16 12:50:02 +0530

    GET/POST REQUEST

--- src/app/comments/route.js
+++ src/app/comments/route.js
@@ -4,3 +4,14 @@ export async function GET() {
     return  Response.json(comments);
     
 }
+
+export async function POST(req) {
+    const comment = await req.json();
+    const newComment = {
+        id: comments.length + 1,
+        text: comment.text,
+    };
+    comments.push(newComment);
+    return Response.json(newComment, { status: 201 });
+}
+
